dd56540e5e7f180b064631f909dba9f815dc7708
David Young <dichlorodiphen@gmail.com>
2024-05-05 16:06:39 -0700

    Fill

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,95 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// maxOffset = 8192
+// offset = 0
+// loop:
+// if !keydown; goto CLEAR
+//    if (offset == maxOffset) goto loop
+//		*(SCREEN+offset)=-1
+//    offset++
+//    goto loop
+// CLEAR:
+//  if (offset == 0) goto loop
+//    *(SCREEN+offset)=0
+//    offset--
+//    goto loop
+
+// maxOffset = 8192
+// offset = 0
+// loop:
+// if RAM[KBD] == 0; goto CLEAR
+//    if (offset == maxOffset) goto loop
+//		*(SCREEN+offset)=-1
+//    offset++
+//    goto loop
+// CLEAR:
+//  if (offset == 0) goto loop
+//    *(SCREEN+offset)=0
+//    offset--
+//    goto loop
+
+// maxOffset = 8192
+@8192
+D=A
+@maxOffset
+M=D
+
+// offset = 0
+@0
+D=A
+@offset
+M=D
+
+(LOOP)
+// if RAM[KBD] == 0, goto CLEAR
+@KBD
+D=M
+@CLEAR
+D;JEQ
+
+// if offset == maxOffset, goto LOOP
+@maxOffset
+D=M
+@offset
+D=D-M
+@LOOP
+D;JEQ
+
+// *(SCREEN + offset) = -1
+@SCREEN
+D=A
+@offset
+A=D+M
+M=-1
+
+// offset = offset + 1
+@offset
+M=M+1
+
+// goto LOOP
+@LOOP
+0;JMP 
+
+(CLEAR)
+// if offset == 0, goto LOOP
+@offset
+D=M
+@LOOP
+D;JEQ
+
+// *(SCREEN+offset) = 0
+@SCREEN
+D=A
+@offset
+A=D+M
+M=0
+
+// offset = offset - 1
+@offset
+M=M-1
+
+// goto LOOP
+@LOOP
+0;JMP
+
