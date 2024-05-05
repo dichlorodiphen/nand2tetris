be8f592df29188b108dbfe557dde7774aa31d625
David Young <dichlorodiphen@gmail.com>
2024-05-05 15:25:49 -0700

    Mult

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// R2 = 0
+@0
+D=A
+@R2
+M=D
+
+// if R1 <= 0, goto STOP
+@R1
+D=M
+@END
+D;JLE
+
+(LOOP)
+// R2 = R2 + R0
+@R0
+D=M
+@R2
+M=M+D
+// R1 = R1 - 1
+@1
+D=A
+@R1
+M=M-D
+// if R1 > 0, goto LOOP
+@R1
+D=M
+@LOOP
+D;JGT
+
+(END)
+@END
+0;JMP
+
